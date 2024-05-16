14eb703f83591b5a1a0962831adcb990a2043f63
Juan Pablo <juanpablomoyano14@gmail.com>
2024-05-15 21:17:51 -0300

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Primer commit</title>
+</head>
+<body>
+    <?php
+        print "Mi primera Pagina";
+    ?>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure molestias eos, pariatur incidunt, similique asperiores enim dignissimos amet eaque, nemo dolorum temporibus ex? Error, qui quisquam facilis sed voluptas animi.
+</body>
+</html>
