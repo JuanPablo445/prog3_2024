783bc0348e415fa33ca000ce021011a767e6b3bf
JP <juanpablomoyano14@gmail.com>
2024-10-22 19:02:34 -0300

    .

--- Horarios/formGetTimetable.php
+++ Horarios/formGetTimetable.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horarios</title>
     <link rel="stylesheet" href="css\timetable.css">
-</head>
-<body>
 
+</head>
+    
+<body>
+<?php include ("./header.footer/header.php"); ?>
 <?php
 if (!isset($_REQUEST["buscar"])) {
 ?>
---
+++ header.footer/header.php
@@ -0,0 +1,48 @@
+g<header class="header">
+    <nav class="nav_container"> 
+        <div class="iconos_menu_container"> 
+            <img src="components/icons/menu-hamburguesa.png" class="menu_icon" id="abrir" alt="" class="menu-icon">
+            <img src="components/icons/cerrar.png" alt="" class="menu-cerrar-icon" id="cerrar">
+        </div>
+        <div class="menu-container" id="menu-container">
+                    <div class="menu-item-container" id="menu-item-container">
+                        
+                        <div class="c1">
+                            <a href="index.php" class="menu-item"><img src="components/icons/home.png" alt="" class="icons"><span>Inicio</span></a>
+                        </div>
+
+                        <div class="c1">    
+                            <a href="" class="menu-item"><img src="components/icons/colegio.png" alt="" class="icons"> <span>Historia</span></a>
+                        </div>
+
+                        <div class="c1"> 
+                            <a href="LoginStudent.php" class="menu-item"><img src="components/icons/gorro-de-graduacion.png" alt="" class="icons"> <span>Estudiante/tutor</span></a>
+                        </div>
+
+                        <div class="c1">    
+                            <a href="ayuda.html" class="menu-item"><img src="components/icons/ayuda.png" alt="" class="icons"> <span>Ayuda</span></a>
+                        </div>
+
+                        <div class="c1">
+                            <a href="noticias.html" class="menu-item"><img src="components/icons/mundo.png" alt="" class="icons"> <span>Noticias</span></a>
+                        </div>
+
+                        <div class="c1">
+                            <a href="loginTeacher.php" class="menu-item"><img src="components/icons/usuario.png" alt="" class="icons"> <span>Gestión Docente</span></a>
+                        </div>
+
+                        <div class="c1">
+                            <a href="planificacion.php" class="menu-item"><img src="components/icons/libro.png" alt="" class="icons"> <span>Planificaciones</span></a>
+                        </div>
+
+                        <div class="c1">
+                            <a href="welcome.php" class="menu-item"><img src="components/icons/curricular.png" alt="" class="icons"> <span>Estructura Curricular</span></a>
+                        </div>
+
+
+                </div>
+        </div>
+</nav>
+</header>
+
+
